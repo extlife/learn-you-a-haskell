e9b429bd8e06c7292dc020a32e58178597b91971
extlife <extlife@yandex.ru>
2022-03-25 16:29:02 +0300

    Add Chapter 05

---
+++ src/Ch05/Core.hs
@@ -0,0 +1,74 @@
+module Ch05.Core where
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/ 10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A' .. 'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x : xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x : xs)
+    | f x = x : filter' f xs
+    | otherwise = filter' f xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter f [100000, 99999 ..])
+    where f x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | otherwise = n : chain (n * 3 + 1)
+
+numLongChain :: Int
+numLongChain = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
+
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x = foldl (\acc y -> if x == y then True else acc) False
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f = foldr (\x acc -> f x : acc) []
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' f = foldr (\x acc -> if f x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+and' :: [Bool] -> Bool 
+and' = foldr (&&) True
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1 ..]))) + 1
